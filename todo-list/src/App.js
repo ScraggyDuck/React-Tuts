3d0e8e810f867d9213377bdd8a8d0e6f0dbae542
JokerV <Quocvietcoi2000@gmail.com>
2019-06-22 16:04:32 +0700

    Render a list

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -1,29 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import TodoItem from './components/TodoItem';
 
 function App() {
+  const todoItems = [
+    'Học bài',
+    'Đi xem phim',
+    'Làm đồ án'
+  ];
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <TodoItem title="Đi chợ" />
-        <TodoItem title="Đi học" />
-        <TodoItem title="Đi làm" />
-        <TodoItem title="Đi chơi với người yêu" />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        todoItems.map((item, index) => <TodoItem title={item} key={index} />)
+      }
     </div>
   );
 }
