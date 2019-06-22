cbf78b65d675560d317d0b21f617df6e39516e74
JokerV <Quocvietcoi2000@gmail.com>
2019-06-22 14:56:21 +0700

    TodoList Components

---
+++ todo-list/src/components/TodoItem.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class TodoItem extends Component {
+    render(){
+        return (
+            <div className="TodoItem">
+                <p>
+                    Di cho
+                </p>
+            </div>
+        );
+    }
+}
+
+export default TodoItem;
