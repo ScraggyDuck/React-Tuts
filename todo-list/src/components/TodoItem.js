e871b46ddfd94fb575445300f1ceb05f89b44d48
JokerV <Quocvietcoi2000@gmail.com>
2019-06-23 13:35:41 +0700

    Finish todoList part 2

--- todo-list/src/components/TodoItem.js
+++ todo-list/src/components/TodoItem.js
@@ -1,15 +1,22 @@
 import React, {Component} from 'react';
 import classNames from 'classnames';
 
+import checkImg from '../img/check.svg';
+import checkCompleteImg from '../img/check-complete.svg';
 import './TodoItem.css'
 
 class TodoItem extends Component {
     render(){
         const { item, onItemClick } = this.props;
+        let url = checkImg;
+        if(item.isComplete){
+            url = checkCompleteImg;
+        }
         return (
-            <div onClick={onItemClick} className={classNames('TodoItem', {
+            <div className={classNames('TodoItem', {
                 'TodoItem-complete': item.isComplete
             })}>
+                <img onClick={onItemClick} src={url} width={32} height={32} />
                 <p>{item.title}</p>
             </div>
         );
